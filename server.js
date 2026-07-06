1400979e9568980797fdf44d134017f16afea8c9
Pranav Bhapkar <pranavbhapkar338@gmail.com>
2026-07-06 12:28:42 +0530

    Add static file serving and index.html route to server

--- server.js
+++ server.js
@@ -44,3 +44,14 @@ app.listen(3000, () => {
     console.log("Server running on http://localhost:3000");
 
 });
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(__dirname));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
